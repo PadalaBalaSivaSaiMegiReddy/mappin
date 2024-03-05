47f0bfb091092c491a0252723b3b1b1476654749
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-05 10:29:15 +0530

    Update MongoDB URL and add database connection handling

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const dotenv = require("dotenv");
+const path = require("path");
+const cors =require("cors");
 
 dotenv.config();
 const app=express();
 app.use(express.json());
 
-app.listen(process.env.PORT,()=>{
-    console.log(`App is listening to port ${process.env.PORT}`)
-})
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("App connected to database");
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening to port ${process.env.PORT}`)
+    })
+}).catch((error) => {
+    console.log(error);
+});
+
+
+
