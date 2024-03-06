7c19270f0e44700021df816f3002819a01035f33
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-06 12:35:13 +0530

    Add CORS and fetch pins from API

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ const cors =require("cors");
 const pinRoute=require("./routes/pins")
 const usersRoute=require("./routes/users")
 
+
+//use cors 
+
 dotenv.config();
 const app=express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("App connected to database");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,30 @@ import Map,{Marker,Popup} from 'react-map-gl';
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import 'mapbox-gl/dist/mapbox-gl.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { format} from 'timeago.js'
 
 export default function App() {
+  const currentUser="Jane";
+  const [pins,setPins]=useState([]);
+  const[currentPlaceId,setCurrentPlaceId]=useState(null);
+  const handleMarkerClick=(id)=>{
+    setCurrentPlaceId(id);
+  }
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("http://localhost:8800/api/pins");
+        setPins(allPins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+  console.log(pins)
   return <Map
   mapboxAccessToken={import.meta.env.VITE_APP_MAPBOX}
   initialViewState={{
@@ -16,29 +37,65 @@ export default function App() {
   style={{width: "100vw", height: "100vh"}}
   mapStyle="mapbox://styles/mapbox/streets-v9"
 >
-<Marker longitude={78.042068} latitude={27.173891} anchor="bottom" >
-  {/* <img src='./public/pin-icon'/> */}
-  <FaMapMarkerAlt style={{fontSize:20,color:"slateblue"}}/>
-    </Marker>
-    {/* <Popup longitude={78.042068} latitude={27.173891} closeButton={true} closeOnClick={false}
-        anchor="left">
-        <div className="card">
-          <label>Place</label>
-          <h4 className='place'>Taj Mahal</h4>
-          <label>Review</label>
-          <p className="desc">Beautiful place. I like it.</p>
-          <label>Rating</label>
-          <div className="stars">
-          <FaStar className='star'/>
-          <FaStar className='star'/>
-          <FaStar className='star'/>
-          <FaStar className='star'/>
-          <FaStar className='star'/>
+
+  {pins.map((p)=>
+<>
+
+  <Marker longitude={p.long} latitude={p.lat} anchor="bottom" >
+    <FaMapMarkerAlt onClick={()=>handleMarkerClick(p._id)} style={{fontSize:20,color:p.username===currentUser?"tomato":"slateblue"}}/>
+    {p._id===currentPlaceId &&
+    
+    (<Popup longitude={p.long} latitude={p.lat} closeButton={true} closeOnClick={false}
+    anchor="left">
+          <div className="card">
+            <label>Place</label>
+            <h4 className='place'>{p.title}</h4>
+            <label>Review</label>
+            <p className="desc">{p.desc}</p>
+            <label>Rating</label>
+            <div className="stars">
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>{p.username}</b></span>
+            <span className="date">{format(p.createdAt)}</span>
           </div>
-          <label>Information</label>
-          <span className="username">Created by <b>Megi</b></span>
-          <span className="date">1 hour ago</span>
-        </div>
-      </Popup> */}
+        </Popup>)
+          }
+      </Marker>
+</>
+
+
+  )}
+{/* <>
+<Marker longitude={28} latitude={30} anchor="bottom" >
+    <FaMapMarkerAlt style={{fontSize:20,color:"slateblue"}}/>
+      </Marker>
+      <Popup longitude={78.042068} latitude={27.173891} closeButton={true} closeOnClick={false}
+          anchor="left">
+          <div className="card">
+            <label>Place</label>
+            <h4 className='place'>Taj Mahal</h4>
+            <label>Review</label>
+            <p className="desc">Beautiful place. I like it.</p>
+            <label>Rating</label>
+            <div className="stars">
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            <FaStar className='star'/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>Megi</b></span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup>
+        </> */}
+
 </Map>
 }
