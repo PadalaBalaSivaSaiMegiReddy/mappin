4d7aa71bee23986bb5d7da510ec77fe9e0cb2263
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-07 16:30:41 +0530

    Add button styles and update map layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ export default function App() {
 
   console.log(newPlace && newPlace.lat)
   return (
-    <Map
+<div style={{ height: "100vh", width: "100%" }}>    <Map
       mapboxAccessToken={import.meta.env.VITE_APP_MAPBOX}
       initialViewState={{
         longitude: defaultLong,
@@ -129,31 +129,12 @@ export default function App() {
             </div>
 </Popup>
 }
-{/* <>
-<Marker longitude={28} latitude={30} anchor="bottom" >
-<FaMapMarkerAlt style={{fontSize:20,color:"slateblue"}}/>
-</Marker>
-<Popup longitude={78.042068} latitude={27.173891} closeButton={true} closeOnClick={false}
-anchor="left">
-<div className="card">
-<label>Place</label>
-<h4 className='place'>Taj Mahal</h4>
-<label>Review</label>
-<p className="desc">Beautiful place. I like it.</p>
-<label>Rating</label>
-            <div className="stars">
-            <FaStar className='star'/>
-            <FaStar className='star'/>
-            <FaStar className='star'/>
-            <FaStar className='star'/>
-            <FaStar className='star'/>
-            </div>
-            <label>Information</label>
-            <span className="username">Created by <b>Megi</b></span>
-            <span className="date">1 hour ago</span>
-          </div>
-        </Popup>
-        </> */}
 
-</Map>)
+<Popup latitude={82} longitude={-169} 
+          anchor="left">
+        <button className='button logout'>Logout</button>
+        <button className='button login'>Login</button>
+        <button className='button register'>Register</button></Popup>
+</Map>
+</div>)
 }
